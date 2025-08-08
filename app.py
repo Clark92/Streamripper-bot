add1d33d0f292931f32eb4035ac595bf0081d2f3
Clark92 <96780536+Clark92@users.noreply.github.com>
2025-08-08 14:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 print("Bot is running...")
+import logging
+from telegram.ext import Updater, CommandHandler
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+def start(update, context):
+    update.message.reply_text("👋 Hello! Bot is working.")
+
+def main():
+    TOKEN = "8304683093:AAEj7w-On9532Y_650KPbmQUBc8u31qb48o"
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
